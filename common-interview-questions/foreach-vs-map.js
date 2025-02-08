3e509e03cc93f424bb56b283028173845a7001f2
sachin-preplaced <sachin.arya@preplaced.in>
2025-02-08 09:21:51 +0530

    map return new array, foreach does not

---
+++ common-interview-questions/foreach-vs-map.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3]
+console.log(arr.map((it)=>it*2)) // map  creates and returns a new array by 
+// performing some specific operation on every element of the array
+
+console.log(arr.forEach((item)=>item*4)) // foreach also perform some specific operation on every element 
+// of array but it does not create  a new array and also does not return any thing
