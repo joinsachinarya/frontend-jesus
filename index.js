dbac01609529c4b106b24e19054babbd5a2b2ee0
joinsachinarya <sachinkarya333@gmail.com>
2025-02-22 11:54:40 +0530

    node js crypto lib

---
+++ index.js
@@ -0,0 +1,9 @@
+const { createHmac } = require("node:crypto")
+const name = "day1";
+try {
+    const hash = createHmac("sha256", name).digest("hex")
+    console.log("HASH", hash)
+} catch (e) {
+    console.error(e);
+}
+
