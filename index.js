74d3b1484119ac0a00f1de22435fccecf6bb0494
joinsachinarya <sachinkarya333@gmail.com>
2025-02-22 17:32:37 +0530

    hash ass 2

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const { createHmac } = require("node:crypto")
-const name = "day1";
-try {
-    const hash = createHmac("sha256", name).digest("hex")
-    console.log("HASH", hash)
-} catch (e) {
-    console.error(e);
-}
+const crypto = require("crypto")
+// const name = "day1";
+// try {
+//     const hash = createHmac("sha256", name).digest("hex")
+//     console.log("HASH", hash)
+// } catch (e) {
+//     console.error(e);
+// }
 
 
 
-function findHasWhichStartWith() {
+function findHasWhichStartWith(prefix) {
     let input = 0;
     while (true) {
-        const hash = createHmac('sha256', "Hey").update(input.toString()).digest('hex');
-        if (hash.startsWith('0000')){
-            console.log(hash, input.toString())
-            break;
+        let inputStr = `gonna make you proud papa` + input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash };
         }
         input++;
     }
 }
-findHasWhichStartWith()
+console.log(findHasWhichStartWith('0000'))
