7bf62bafb4d7e744388d977c14379044f7fcd8e3
joinsachinarya <sachinkarya333@gmail.com>
2025-04-24 08:19:51 +0530

    promise.customAll added

--- pollyfills/promise.all/promise-all.js
+++ pollyfills/promise.all/promise-all.js
@@ -1,34 +1,53 @@
-const promise1 = () => new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('promise1 resolved')
     }, 1000)
 })
 
-const promise2 = () => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject("promise2 rejected")
-    }, 2000)
-})
+// const promise2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("promise2 rejected")
+//     }, 2000)
+// })
 
 
-const promise3 = () => new Promise((resolve, reject) => {
+const promise3 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("promise2 resolved")
+        resolve("promise3 resolved")
     }, 4000)
 })
 
 
-function sol() {
-    const val2 = promise2();
-    val2.then((res) => console.log("val2", res)).catch((err) => console.log("val2", err)).finally(() => {
-        const val3 = promise3()
-        val3.then((res) => console.log("val3", res)).catch((err) => console.error('val3', err)).finally(() => {
-            const val1 = promise1()
-            val1.then((res) => console.log("val1", res)).catch((err) => console.err('val1', err)).finally(() => {
-                console.log("All async promise functions called")
+const finalVal = Promise.all([promise1,
+    // promise2,
+    promise3
+])
+
+
+
+
+function customAll(promises) {
+    return new Promise((resolve, reject) => {
+        let ans = [];
+        promises.forEach((p, i) => {
+            p.then((res) => {
+                ans[i] = res;
+                if (ans.length === promises.length) {
+                    resolve(ans)
+                }
+            }).catch((err) => {
+                reject(err);
             })
         })
     })
 }
 
-sol();
+Promise.customAll = customAll
+
+const finalVal2 = Promise.customAll([promise1,
+    // promise2,
+    promise3
+])
+
+
+console.log(finalVal.then((res1) => console.log("res1", res1)), finalVal2.then((res2) => console.log("res2", res2)))
