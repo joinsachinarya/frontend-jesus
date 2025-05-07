136438986d498fd2eca0439fa0b181e3323ed6ea
Sachin Arya <67674344+joinsachinarya@users.noreply.github.com>
2025-05-07 10:57:20 +0530

    Create prototype.ts

---
+++ common-interview-questions/prototype.ts
@@ -0,0 +1,3 @@
+// In javascript every object has a hidden key named [[Prototype]] which can be either null or references another object,
+// that object is called 'prototype'. 
+// We can access this prototype using obj.__proto__ or Object.getPrototypeOf(obj).
